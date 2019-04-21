7d6e19146f90778784c016c7dc4bc96882bafa1a
xiangsxuan <598582847@qq.com>
2019-04-22 03:30:50 +0800

    添加ITodoItemService实现, 此时暂以假数据代替, 尚未用数据库

--- AspNetCoreTodo/Startup.cs
+++ AspNetCoreTodo/Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.EntityFrameworkCore;
 using AspNetCoreTodo.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using AspNetCoreTodo.Services;
 
 namespace AspNetCoreTodo
 {
@@ -28,6 +29,7 @@ namespace AspNetCoreTodo
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<ITodoItemService, FakeTodoItemService>();
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
